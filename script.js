0ff4d18e1e6e37ce8f385b4fb6fda11f6cd98cac
hihello <13111181+hihellos@users.noreply.github.com>
2020-08-08 21:10:49 -0400

    html set up for second and third ajax call info to be appended

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
     console.log("ready");
 
+    var key = "b0a1a1ca5591877ea2e9552cf91da345"
+
     // AJAX call to OpenWeather API - Current Weather Data
     function searchCityWeather(city) {
-        var key = "b0a1a1ca5591877ea2e9552cf91da345"
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + key;
 
         $.ajax({
@@ -16,16 +17,25 @@ $(document).ready(function() {
             $("#searchedCityDisplay").append(cityName);
 
             var tempNow = response.main.temp;
-            $("#temp-now").append(Math.round(tempNow) + "&deg;");
+            $("#temp-now").append(Math.round(tempNow) + "&deg; F");
 
-            // var humidityNow = response.main.humidity;
+            var humidityNow = response.main.humidity;
+            $("#humid-now").append(humidityNow + "&percnt;");
 
-            // var windSpeedNow = response.wind.speed;
+            var windSpeedNow = response.wind.speed;
+            $("#wind-now").append(Math.round(windSpeedNow) + " MPH");
 
+            // lat.push(response.coord.lat)
         });
     }
 
-    // AJAX call - UV Index
+    // var lat = [];
+    // var lon = "";
+
+    // // AJAX call - UV Index
+    // function searchUVindex(uv) {
+    //     var queryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + key + "&lat=" + lat + "&lon=" + lon;
+    // }
 
     // AJAX Call - 5-Day Forecast
 
@@ -39,4 +49,9 @@ $(document).ready(function() {
         searchCityWeather(inputCity);
 
     });
+
+    // Use moment.js to display todays day nex tto current city, and next 5 days in forecast
+
+    // Store search history of cities
+
 });

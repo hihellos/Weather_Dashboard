43276286ea9de017b66e88373a4d5d658fd95e0e
hihello <13111181+hihellos@users.noreply.github.com>
2020-08-08 22:27:41 -0400

    local storage working for past cities searched, current search effectively overwriting page content

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ $(document).ready(function() {
 
     // AJAX call to OpenWeather API - Current Weather Data
     function searchCityWeather(city) {
+        $("#searchedCityDisplay").text("");
+        $("#temp-now").text("");
+        $("#humid-now").text("");
+        $("#wind-now").text("");
+
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + key;
 
         $.ajax({
@@ -17,7 +22,7 @@ $(document).ready(function() {
             $("#searchedCityDisplay").append(cityName);
 
             var tempNow = response.main.temp;
-            $("#temp-now").append(Math.round(tempNow) + "&deg; F");
+            $("#temp-now").append(Math.round(tempNow) + "&deg;" + " F");
 
             var humidityNow = response.main.humidity;
             $("#humid-now").append(humidityNow + "&percnt;");
@@ -52,6 +57,60 @@ $(document).ready(function() {
 
     // Use moment.js to display todays day nex tto current city, and next 5 days in forecast
 
-    // Store search history of cities
+    // LOCAL STORAGE - Store search history of cities
+    var cityInput = $("#city-input");
+    var searchForm = $("#search-btn");
+    var pastCityList = $("#past-cities");
 
+    var cities = [];
+
+    init();
+
+    // Render past cities
+    function renderCities() {
+        pastCityList.html("");
+
+        for (var i = 0; i< cities.length; i++) {
+            var city = cities[i];
+
+            var li = $("<li>");
+            li.text(city);
+            li.attr("city-index", i);
+
+            pastCityList.prepend(li);
+        }
+    }
+
+    // Get stored cities from local storage
+    function init() {
+        var storedCities = JSON.parse(localStorage.getItem("cities"));
+
+        if (storedCities !== null) {
+            cities = storedCities;
+        }
+        renderCities();
+    }
+
+    function storeCities() {
+        localStorage.setItem("cities", JSON.stringify(cities));
+    }
+
+    // When a city is searched
+    searchForm.on("click", function(event) {
+        event.preventDefault();
+
+        var cityText = cityInput.val().trim();
+
+        if (cityText === "") {
+            return;
+        }
+
+        cities.push(cityText);
+        $("#city-input").html("");
+
+        storeCities();
+        renderCities();
+    });
+
+    // When a city in the search history is clicked
 });
